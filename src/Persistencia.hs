93ac9c064f5afbf73a1f387e0bec2e1d00a7eb4a
Gfachini02 <gabrielfachinirp@gmail.com>
2025-04-24 16:07:07 -0300

    Update Persistencia.hs

--- src/Persistencia.hs
+++ src/Persistencia.hs
@@ -1,8 +1,10 @@
 module Persistencia(salvarEmArquivo, carregarDeArquivo) where
 import Tipos
-salvarEmArquivo :: FilePath -> [Tarefa] -> IO ()
+--salva as tarefas adicionadas em um arquivo txt com o nome especificado pelo usuario
+salvarEmArquivo :: FilePath -> [Tarefa] -> IO () 
 salvarEmArquivo caminho tarefas = writeFile caminho (show tarefas)
 
+--le o arquivo txt especificado e adiciona ele a tarefa
 carregarDeArquivo :: FilePath -> IO [Tarefa]
 carregarDeArquivo caminho = do
    conteudo <- readFile caminho
